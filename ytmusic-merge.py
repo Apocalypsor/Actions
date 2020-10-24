9a218e2b3214800517ee153be1c2bea916eda8e2
Apocalypsor <Apocalypsor@users.noreply.github.com>
2020-10-24 15:50:35 +0800

    Update

---
+++ ytmusic-merge.py
@@ -0,0 +1,26 @@
+from ytmusicapi import YTMusic
+import os
+
+playlistId = os.environ['YT_PLAYLIST']
+
+ytmusic = YTMusic('auth.json')
+
+songs = ytmusic.get_library_songs(limit=0, validate_responses=True) + ytmusic.get_library_upload_songs(limit=0)
+songs = [songs['videoId'] for songs in songs]
+
+playlistSongs = ytmusic.get_playlist(playlistId=playlistId, limit=0)['tracks']
+
+remove_songs, add_songs = [], []
+for p in playlistSongs:
+    if p not in songs:
+        remove_songs.append(p)
+
+for s in songs:
+    if s not in playlistSongs:
+        add_songs.append(s)
+
+if remove_songs:
+    ytmusic.remove_playlist_items(playlistId=playlistId, videos=removeSongs)
+
+if add_songs:
+    ytmusic.add_playlist_items(playlistId=playlistId, videoIds=addSongs)
