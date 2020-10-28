c81f03885a4f8615b673b749da5b6bf2c7c5bebd
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2020-10-28 23:01:47 +0800

    Update

--- ytmusic-merge.py
+++ ytmusic-merge.py
@@ -1,33 +1,49 @@
 from ytmusicapi import YTMusic
 import os
+import sys
 
-playlist_id = os.environ['YT_PLAYLIST']
+playlist_id = os.environ["YT_PLAYLIST"]
+limit = sys.maxsize
 
-ytmusic = YTMusic('auth.json')
+ytmusic = YTMusic("auth.json")
 
-songs = ytmusic.get_library_songs(limit=0, validate_responses=True) + ytmusic.get_library_upload_songs(limit=0)
-songs = [s['videoId'] for s in songs]
+songs = ytmusic.get_library_songs(
+    limit=limit, validate_responses=True
+) + ytmusic.get_library_upload_songs(limit=limit)
+songs = dict((s["videoId"], s["title"]) for s in songs)
 
-playlist_songs = ytmusic.get_playlist(playlistId=playlist_id, limit=0)['tracks']
-playlist_songs_id = [p['videoId'] for p in playlist_songs]
+playlist_songs = ytmusic.get_playlist(playlistId=playlist_id, limit=0)["tracks"]
+playlist_songs = dict((p["videoId"], p) for p in playlist_songs)
 
 remove_songs, add_songs = [], []
 for p in playlist_songs:
-    if p['videoId'] not in songs:
+    if p not in songs:
         remove_songs.append(p)
 
 for s in songs:
-    if s not in playlist_songs_id:
+    if s not in playlist_songs:
         add_songs.append(s)
 
 if remove_songs:
-    print('------------------------------')
-    print('Remove Songs:',remove_songs)
+    print("------------------------------")
+    print("[Remove Songs]")
+    remove_title = [playlist_songs[s]["title"] for s in remove_songs]
+    remove_songs = [
+        {
+            "videoId": playlist_songs[s]["videoId"],
+            "setVideoId": playlist_songs[s]["setVideoId"],
+        }
+        for s in remove_songs
+    ]
+    print("\n".join(remove_title))
     ytmusic.remove_playlist_items(playlistId=playlist_id, videos=remove_songs)
 
 if add_songs:
-    print('------------------------------')
-    print('Add Songs:', add_songs)
+    print("------------------------------")
+    print("[Add Songs]")
+    add_title = [songs[s] for s in add_songs]
+    print("\n".join(add_title))
     ytmusic.add_playlist_items(playlistId=playlist_id, videoIds=add_songs)
 
-print('Done!')
+print("------------------------------")
+print("Done!")
