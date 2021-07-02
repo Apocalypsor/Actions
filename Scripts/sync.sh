855a42a85a2514ce69b32e2296e1363cd428aa46
Apocalypsor <37431235+Apocalypsor@users.noreply.github.com>
2021-07-02 22:50:53 +0800

    Update sync.sh

--- Scripts/sync.sh
+++ Scripts/sync.sh
@@ -28,21 +28,27 @@ find . -maxdepth 1 -path ./.git -prune -o -exec rm -rf {} \; 2> /dev/null
 echo
 echo "--------------------------Cloning source repo-------------------------"
 git clone --depth 1 -b $source_branch $source_repo /tmp/source_repo
-rm -rf /tmp/source_repo/.git/
+if [ -d "/tmp/source_repo" ]; then
+    rm -rf /tmp/source_repo/.git/
 
-echo
-echo "-----------------------------Syncing repo-----------------------------"
-mv -f /tmp/source_repo/* /tmp/dest_repo
-rm -f README.md
-commit
+    echo
+    echo "-----------------------------Syncing repo-----------------------------"
+    mv -f /tmp/source_repo/* /tmp/dest_repo
+    rm -f README.md
+    commit
 
-echo
-if $update
-then
-    echo "Repo updated!"
-    rm -rf /tmp/dest_repo /tmp/source_repo
-    echo "update" > /tmp/update
+    echo
+    if $update
+    then
+        echo "Repo updated!"
+        rm -rf /tmp/dest_repo /tmp/source_repo
+        echo "update" > /tmp/update
+    else
+        echo "Repo already up-to-date!"
+        rm -rf /tmp/dest_repo /tmp/source_repo
+    fi
 else
-    echo "Repo already up-to-date!"
-    rm -rf /tmp/dest_repo /tmp/source_repo
+    echo
+    echo "Error source repo!"
 fi
+
